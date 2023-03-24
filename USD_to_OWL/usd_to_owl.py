36d9225d4977a956e0f3e3722bf1b0cc870a4704
Giang Nguyen <hoanggia@uni-bremen.de>
2023-03-24 14:13:28 +0100

    Update

--- USD_to_OWL/usd_to_owl.py
+++ USD_to_OWL/usd_to_owl.py
@@ -47,29 +47,28 @@ def usd_to_owl(file_path: str) -> None:
         class IslandCover(soma_onto.DesignedComponent):
             pass
 
-        class Window(soma_onto.DesignedComponent):
-            pass
-
         class WindowFrame(soma_onto.Rack):
-            pass
-
-        class WaterTab(soma_onto.DesignedComponent):
             pass
     
     with ABox_onto:
         for prim_name, prim_class in prim_classes.items():
-            if len(onto:=TBox_onto.search(iri='*'+prim_class)) == 1:
-                onto[0](prim_name)
+            if (onto:=eval('TBox_onto.' + prim_class)) != None or (onto:=eval('soma_onto.' + prim_class)) != None or (onto:=eval('soma_home_onto.' + prim_class)):
+                onto(prim_name)
             else:
-                print(prim_class, 'not found, create a new class')
-                prim_child_classes = re.findall('[A-Z][^A-Z]*', prim_class)
-                prim_child_classes.reverse()
-                for prim_child_class in prim_child_classes:
-                    if (onto:=TBox_onto.search_one(iri='*'+prim_child_class)) != None:
-                        with TBox_onto:
-                            exec('class %s(%s):\n\tpass' %(prim_class, 'soma_onto.' + str(onto).split('.')[1]))
-                        eval('%s("%s")' %(prim_class, prim_name))
+                prim_class_list = re.findall('[A-Z][^A-Z]*', prim_class)
+                prim_class_list.reverse()
+
+                prim_class_to_search = ''
+                for prim_class_element in prim_class_list:
+                    prim_class_to_search = prim_class_to_search + prim_class_element
+                    if (onto:=eval('TBox_onto.' + prim_class_to_search)) != None or (onto:=eval('soma_onto.' + prim_class_to_search)) != None or (onto:=eval('soma_home_onto.' + prim_class_to_search)):
                         break
+                if onto == None:
+                    onto = TBox_onto.search_one(iri='*DesignedComponent')
+                with TBox_onto:
+                    exec('class %s(%s):\n\tpass' %(prim_class, 'onto'))
+                eval('%s("%s")' %(prim_class, prim_name))
+                print('Create new class\t%s <-- %s' %(TBox_onto.search_one(iri='*'+prim_class), str(onto)))
 
     TBox_onto.save(file=onto_path[0] + '/TBox.owl')
     ABox_onto.save(file=onto_path[0] + '/ABox.owl')
